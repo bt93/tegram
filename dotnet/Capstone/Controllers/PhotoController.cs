2f8bdccbab69fb8b73dfb606b60063cdfd1b0a2c
Zachary Williamson <lacew@protonmail.com[ALace Williamson>
2020-08-03 14:05:03 -0400

    DAO and Controller development

---
+++ dotnet/Capstone/Controllers/PhotoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.DAO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        private readonly IPhotoDAO photoDAO;
+
+        public PhotoController (IPhotoDAO _photoDAO)
+        {
+            photoDAO = _photoDAO;
+        }
+
+        [HttpGet("")]
+        public IActionResult GetPhotos()
+        {
+            List<string> paths = new List<string>();
+            paths = photoDAO.GetAllPhotos();
+            return Ok(paths);
+
+        }
+    }
+}
---
+++ dotnet/Capstone/DAO/PhotoSqlDAO.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Capstone.Models;
+
+
+namespace Capstone.DAO
+{
+    public class PhotoSqlDAO : IPhotoDAO
+    {
+        private readonly string connectionString;
+
+        public PhotoSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public List<string> GetAllPhotos()
+        {
+            List<string> filePaths = new List<string> ();
+            
+            // Hardcoded Values for Testing
+            filePaths.Add(@"C:\Users\Student\git\c-final-capstone-te-gram\dotnet\Capstone\photos\3408.jpg");
+            filePaths.Add(@"C:\Users\Student\git\c-final-capstone-te-gram\dotnet\Capstone\photos\dogs_1280p_0.jpg");
+            filePaths.Add(@"C:\Users\Student\git\c-final-capstone-te-gram\dotnet\Capstone\photos\dog-tales-img.jpg");
+            filePaths.Add(@"C:\Users\Student\git\c-final-capstone-te-gram\dotnet\Capstone\photos\pearl_16x9.jpg");
+            // End of Hardcoded Values for Testing
+
+
+/*            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT file_path FROM photos", conn); //Select every single file path stored in the photos table
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        filePaths.Add(Convert.ToString(reader["file_path"]));
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }*/
+
+            return filePaths;
+        }
+
+    }
+}
+
+
