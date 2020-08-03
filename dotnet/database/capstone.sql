50065137c629b3c1651e4bfcad2a91e5c5d8b16f
Seth Boyle <s.boyle987@gmail.com>
2020-08-03 11:42:09 -0400

    Created database table for photos and inserted a photo into directory

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -16,16 +16,26 @@ GO
 
 --create tables
 CREATE TABLE users (
-	user_id int IDENTITY(1,1) NOT NULL,
+	user_id int IDENTITY(1,1)NOT NULL,
 	username varchar(50) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	salt varchar(200) NOT NULL,
-	user_role varchar(50) NOT NULL
+	user_role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+)
+CREATE TABLE photos (
+	photo_id int IDENTITY(1,1) NOT NULL,
+	file_path varchar(200) NOT NULL,
+	like_count int DEFAULT 0 NOT NULL,
+	user_id int NOT NULL,
+	CONSTRAINT PK_photo PRIMARY KEY (photo_id),
+	CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id)
 )
 
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
-
+INSERT INTO photos (file_path, user_id) VALUES ('C:\Users\Student\git\c-final-capstone-te-gram\dotnet\Capstone\UserPhotos\domestic-dog_thumb.jpg', 1);
 GO
+
+select * from photos
