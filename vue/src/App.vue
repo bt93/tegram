e80d73670aaa24956185099e1e6620d1d2997d52
Jason Howie <howiejason93@gmail.com>
2020-08-03 11:28:29 -0400

    Styling to login and registration page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:ital,wght@0,300;0,500;1,400&display=swap');
+
+body {
+  font-family: 'Roboto', Arial, sans-serif;
+}
+
+.text-center {
+  text-align: center;
+  padding: 0 1vh 0 1vh;
+  border: 1px solid rgba(var(--b6a,219,219,219),1);
+}
+
+.text-center > form {
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0;
+}
+
+.text-center > form > h1 {
+  font-family: 'Pacifico', cursive;;
+}
+</style>
