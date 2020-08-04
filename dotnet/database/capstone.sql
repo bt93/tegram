a6193ebcddd45c5083de4c9ee8ab09b4549c57fd
Zachary Williamson <lacew@protonmail.com[ALace Williamson>
2020-08-04 10:23:07 -0400

    Added additional files to the SQL database

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -38,13 +38,37 @@ INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Lace','Jg4
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Porter','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Jason','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Seth','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+INSERT INTO users (username, password_hash, salt, user_role) VALUES ('Val','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
+
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 INSERT INTO photos (file_path, user_id) VALUES ('/v1596548892/TE-Gram/pearl_16x9_tqkcko.jpg', 1);
 INSERT INTO photos (file_path, user_id) VALUES ('/v1596548892/TE-Gram/3408_xxawqk.jpg', 2);
 INSERT INTO photos (file_path, user_id) VALUES ('/v1596548892/TE-Gram/dogs_1280p_0_ofuwsx.jpg', 3);
 INSERT INTO photos (file_path, user_id) VALUES ('/v1596548895/TE-Gram/domestic-dog_thumb_yacyjk.jpg', 4);
 INSERT INTO photos (file_path, user_id) VALUES ('/v1596548892/TE-Gram/dogs_1280p_0_ofuwsx.jpg', 4);
-INSERT INTO photos (file_path, user_id) VALUES ('C:\Users\Student\git\c-final-capstone-te-gram\dotnet\Capstone\photos\pearl_16x9.jpg', 5);
+
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549728/TE-Gram/TEworkplacePhoto14_icubge.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549742/TE-Gram/TEworkplacePhoto16_m3kz0t.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549764/TE-Gram/TEworkplacePhoto17_hrf4de.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549781/TE-Gram/TEworkplacePhoto15_k56p8v.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549791/TE-Gram/TEworkplacePhoto18_t5weuz.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549798/TE-Gram/TEworkplacePhoto20_mopxyq.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549813/TE-Gram/TEworkplacePhoto08_a5mujf.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549813/TE-Gram/TEworkplacePhoto19_msw98r.png', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549818/TE-Gram/TEworkplacePhoto01_ijjkfl.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549843/TE-Gram/TEworkplacePhoto21_l4cehq.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549847/TE-Gram/TEworkplacePhoto02_tvoubo.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549853/TE-Gram/TEworkplacePhoto09_gg56dq.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549856/TE-Gram/TEworkplacePhoto10_txpocn.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549876/TE-Gram/TEworkplacePhoto13_kjb9az.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549876/TE-Gram/TEworkplacePhoto03_jxhqpd.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549918/TE-Gram/TEworkplacePhoto06_prwc8s.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549942/TE-Gram/TEworkplacePhoto05_i6qofg.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549943/TE-Gram/TEworkplacePhoto07_h1oyoq.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549961/TE-Gram/TEworkplacePhoto12_hf6obs.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549962/TE-Gram/TEworkplacePhoto04_akg4dc.jpg', 6);
+INSERT INTO photos (file_path, user_id) VALUES ('/v1596549856/TE-Gram/TEworkplacePhoto10_txpocn.jpg', 6);
+
 GO
 
 select * from photos
