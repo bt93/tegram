f1115b4b6a351f9609c967bc7ba579c57f6bf87b
Jason Howie <howiejason93@gmail.com>
2020-08-04 10:03:00 -0400

    Added user page

--- vue/src/services/PhotoService.js
+++ vue/src/services/PhotoService.js
@@ -3,5 +3,9 @@ import axios from 'axios'
 export default {
     getAllPhotos() {
         return axios.get('/photo');
-    }   
+    },
+
+    getPhotosByUser(id) {
+        return axios.get(`/photo/${id}`);
+    }
 }
--- vue/src/views/User.vue
+++ vue/src/views/User.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="user">
       <h1 class="title">TEGram</h1>
+      <photo-container v-for="photo in photos" :key="photo" :photo="photo"/>
+      <img src="" alt="">
   </div>
 </template>
 
 <script>
+import PhotoContainer from '../components/PhotoContainer'
+import photoService from '../services/PhotoService'
 export default {
-
+  components: {
+    PhotoContainer
+  },
+  data() {
+    return {
+      userId: this.$route.params.id,
+      photos: []
+    }
+  },
+  created() {
+    photoService.getPhotosByUser(this.userId)
+      .then(res => {
+        if (res.status === 200) {
+          res.data.forEach(p => this.photos.push(p));
+        }
+      });
+  }  
 }
 </script>
 
