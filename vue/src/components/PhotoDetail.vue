09058976d1f7d2da5e1761d1793937fd550922a5
Jason Howie <howiejason93@gmail.com>
2020-08-06 11:37:58 -0400

    fix modal

--- vue/src/components/PhotoDetail.vue
+++ vue/src/components/PhotoDetail.vue
@@ -1,12 +1,15 @@
 <template>
-  <modal name="details">
+  <modal name="detail">
       <p>Hello</p>
   </modal>
 </template>
 
 <script>
 export default {
-    name: 'photo-detail'
+    name: 'photo-detail',
+    mounted() {
+        this.$modal.show('detail')
+    }
 }
 </script>
 
